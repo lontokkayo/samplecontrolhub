4365132efdfc623a6a3523a23b58ec9186f18398
Carl William Chua <carlwilliamchua@gmail.com>
2024-01-23 10:45:34 +0900

    Nothing

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
     resolver: {
-      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // Add other extensions if needed
+      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // Add other extensions if necessary
     },
   };
-  
